293165fbca94b45954af10e9188732efe52a2ad5
patnikorn <patnikorn.ta.61@ubu.ac.th>
2020-03-08 20:21:38 +0700

    first commit

---
+++ DatascienceProJect/train.py
@@ -0,0 +1,20 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import fetch_20newsgroups
+import numpy as np
+
+categories = ['comp.os.ms-windows.misc', 'rec.sport.baseball', 'soc.religion.christian']
+
+train = fetch_20newsgroups(subset='train', categories=categories)
+
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+
+model.fit(train.data, train.target)
+
+from joblib import dump
+dump(model, './myapp/static/chatgroup.model')
+dump(train, './myapp/static/train.model')
+
+print(round((model.score(train.data, train.target)*100),2),'%')
+
